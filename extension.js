b145cb7bc79e7d550a115156b02df3b32a35c0e6
ZezeChen <zework@qq.com>
2026-02-03 16:15:01 +0800

    Update extension with latest changes

--- extension.js
+++ extension.js
@@ -1,54 +1,41 @@
 const vscode = require('vscode');
 
-class RecentProvider {
-    constructor() {
-        this._onDidChangeTreeData = new vscode.EventEmitter();
-        this.onDidChangeTreeData = this._onDidChangeTreeData.event;
-    }
-
-    refresh() {
-        this._onDidChangeTreeData.fire();
-    }
-
-    getTreeItem(element) {
-        return element;
-    }
-
-    async getChildren(element) {
-        if (!element) {
-            return [
-                new vscode.TreeItem('文件夹', vscode.TreeItemCollapsibleState.Expanded),
-                new vscode.TreeItem('文件', vscode.TreeItemCollapsibleState.Expanded)
-            ];
-        }
-
-        const { workspaces, files } = await vscode.commands.executeCommand('_workbench.getRecentlyOpened');
-        const config = {
-            '文件夹': { items: workspaces.slice(0, 8), uriKey: 'folderUri', cmd: 'vscode.openFolder' },
-            '文件': { items: files.slice(0, 12), uriKey: 'fileUri', cmd: 'vscode.open' }
-        }[element.label];
-
-        return config?.items.map(item => {
-            const uri = vscode.Uri.parse(item[config.uriKey]);
-            const treeItem = new vscode.TreeItem(uri);
-            treeItem.command = { command: config.cmd, arguments: [uri] };
-            return treeItem;
-        }) || [];
-    }
+async function fetchRecent() {
+    const result = await vscode.commands.executeCommand('_workbench.getRecentlyOpened').catch(() => ({}));
+    return result?.workspaces || [];
 }
 
-function activate(context) {
-    const provider = new RecentProvider();
+const provider = {
+    _emitter: new vscode.EventEmitter(),
+    get onDidChangeTreeData() { return this._emitter.event; },
+    refresh() { this._emitter.fire(); },
+    
+    getTreeItem({ uri, name }) {
+        const item = new vscode.TreeItem(name);
+        item.iconPath = new vscode.ThemeIcon('folder');
+        item.tooltip = uri.fsPath;
+        item.command = { command: 'vscode.openFolder', arguments: [uri] };
+        return item;
+    },
+    
+    async getChildren(element) {
+        if (element) return [];
+        const folders = await fetchRecent();
+        const max = vscode.workspace.getConfiguration('recentFolders').get('maxItems', 10);
+        return folders.slice(0, max).map(folder => {
+            const uri = vscode.Uri.parse(folder.folderUri);
+            const name = uri.fsPath.split(/[\\/]/).pop();
+            return { uri, name };
+        });
+    }
+};
 
-    const view = vscode.window.createTreeView('recentFilesView', { treeDataProvider: provider });
-    view.onDidChangeVisibility(e => e.visible && provider.refresh());
-
-    context.subscriptions.push(
-        view,
-        vscode.commands.registerCommand('recentFilesView.refresh', () => provider.refresh())
+function activate(ctx) {
+    ctx.subscriptions.push(
+        vscode.window.createTreeView('recentFoldersView', { treeDataProvider: provider }),
+        vscode.commands.registerCommand('recentFoldersView.refresh', () => provider.refresh()),
+        vscode.workspace.onDidChangeConfiguration(e => e.affectsConfiguration('recentFolders') && provider.refresh())
     );
 }
 
-function deactivate() { }
-
-module.exports = { activate, deactivate };
+module.exports = { activate };
